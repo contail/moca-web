30a078070852369f5d8254be6bff2997e5fa8d2a
contail <qkrtkdwls2121@gmail.com>
2018-06-18 00:01:46 +0900

    update: show video of my local

--- video/views.py
+++ video/views.py
@@ -26,7 +26,22 @@ def VideoView(req):
         serializer = VideoSerializer(videos, many=True)
         return render(req, 'admin_templates/video/list.html', {"videos" : serializer.data, 'page': page,'pager': pager})
 
-
+@api_view(['GET', 'POST'])
+#TODO 로컬디렉토리에서 비디오 리스트 불러오기
+def VideoView_v2(req):
+    if req.method == 'GET':
+        VIDEO_COUNT_PER_PAGE = 1
+        page = int(req.GET.get('page', 1))
+        path = '/Users/contail/Projects/moca-web/moca-web/video_files'
+        video = search(path)
+        start_page = page - 1
+        total_page = len(video)
+        videos = video[start_page * VIDEO_COUNT_PER_PAGE : (start_page + 1) * VIDEO_COUNT_PER_PAGE]
+        pager = {}
+        pager['current'] = page
+        pager['per_page'] = VIDEO_COUNT_PER_PAGE
+        pager['total_page'] = total_page + VIDEO_COUNT_PER_PAGE
+        return render(req, 'admin_templates/video/list.html', {"videos" : videos, 'page': page,'pager': pager})
 
 def search(dirname):
     video_list = []
@@ -34,5 +49,9 @@ def search(dirname):
     for filename in filenames:
         full_filename = os.path.join(dirname, filename)
         ext = os.path.splitext(full_filename)[-1]
+        title =filename
         if ext == '.mp4':
-            video_list.append(full_filename)
+            video_list.append({
+                'title' : title,
+            })
+    return video_list
