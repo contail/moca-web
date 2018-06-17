25104a675ad097daf698e4eb20eb107d9aae6a52
contail <qkrtkdwls2121@gmail.com>
2018-06-18 00:01:11 +0900

    edit: change video list url

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from rest_framework import routers
 
 from frontend.views import main_index
-from video.views import VideoView
+from video.views import VideoView_v2
 
 urlpatterns = [
     path('', main_index, name='main_index'),
-    path('video', VideoView),
+    path('video', VideoView_v2),
 ]
