3e95facdd26f4de52b50cf0b68f90e89c5a44d82
contail <qkrtkdwls2121@gmail.com>
2018-08-26 09:32:28 +0900

    add bootstrap files

---
+++ static/js/app/payment.js
@@ -0,0 +1,10 @@
+/**
+ * Created by MINSEUNG on 2016. 7. 29..
+ */
+(function() {
+    var a, b, c, d, e, f, g, h, i, j, k;
+    a = this.document;
+    b = function() {
+        alert('11');
+    }
+}).call(this);
